aaed3206b68343ed882d765f56d0ab841424dbc8
Yuriy Yarvinen <yuriy.yarvi@yandex.ru>
2022-08-20 04:11:55 +0300

    change player creation and movement

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ canvas.height = 576;
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-	constructor({ position, velocity }) {
+	constructor({ position, velocity, parameters, keys, lastKey }) {
 		this.position = position;
 		this.velocity = velocity;
-		this.height = 150;
-		this.width = 50;
+		this.parameters = parameters;
+		this.lastKey = lastKey;
+		this.keys = keys;
 	}
 
 	draw() {
@@ -22,54 +23,100 @@ class Sprite {
 		context.fillRect(
 			this.position.x,
 			this.position.y,
-			this.width,
-			this.height
+			this.parameters.width,
+			this.parameters.height
 		)
 	}
 
 	update() {
 		this.draw();
 		this.position.x += this.velocity.x;
-		
-		if(this.position.x + this.width + this.velocity.x >= canvas.width){
-			this.position.x = canvas.width - this.width;
+
+		if (this.position.x + this.parameters.width + this.velocity.x >= canvas.width) {
+			this.position.x = canvas.width - this.parameters.width;
+		}
+		if (this.position.x <= 0) {
+			this.position.x = 0;
 		}
 
 
 		this.position.y += this.velocity.y;
-		
-		if(this.position.y + this.height + this.velocity.y >= canvas.height){
+
+		if (this.position.y + this.parameters.height + this.velocity.y >= canvas.height) {
 			this.velocity.y = 0;
 		}
-		else{
+		else {
 			this.velocity.y += gravity;
 		}
+
+
 	}
 }
 
 const player = new Sprite({
-	position:{
-		x:400,
-		y:200,
+	position: {
+		x: 400,
+		y: 200,
 	},
-	velocity:{
-		x:0,
-		y:10,
+	velocity: {
+		x: 0,
+		y: 10,
 	},
+	parameters: {
+		width: 50,
+		height: 150,
+		jumpHeight: 200
+	},
+	keys:{
+		moveX: {
+			a: {
+				pressed: false
+			},
+			d: {
+				pressed: false
+			},
+		},
+		jump: {
+			w: {
+				pressed: false
+			}
+		}
+	}
 })
 
 const enemy = new Sprite({
-	position:{
-		x:0,
-		y:200,
+	position: {
+		x: 0,
+		y: 200,
 	},
-	velocity:{
-		x:0,
-		y:10,
+	velocity: {
+		x: 0,
+		y: 10,
 	},
+	parameters: {
+		width: 50,
+		height: 150,
+		jumpHeight: 200
+	},
+	keys:{
+		moveX: {
+
+			ArrowLeft: {
+				pressed: false
+			},
+			ArrowRight: {
+				pressed: false
+			},
+		},
+		jump: {
+			ArrowUp: {
+				pressed: false
+			},
+		}
+	}
 })
 
-function animate(){
+function animate() {
 	window.requestAnimationFrame(animate);
 	context.fillStyle = backgroundColor;
 	context.fillRect(
@@ -80,25 +127,56 @@ function animate(){
 	);
 	player.update();
 	enemy.update();
-}
 
-animate();
+	player.velocity.x = 0;
+	enemy.velocity.x = 0;
 
-// key binding
-
-window.addEventListener('keydown', function(event){
-	if(event.key == 'd'){
-		player.velocity.x = 2;
-	}
-	if(event.key == 'a'){
+	if (player.keys.moveX.a.pressed && player.lastKey === 'a') {
 		player.velocity.x = -2;
 	}
-});
-window.addEventListener('keyup', function(event){
-	if(event.key == 'd'){
-		player.velocity.x = 0;
+	else if (player.keys.moveX.d.pressed && player.lastKey === 'd') {
+		player.velocity.x = 2;
 	}
-	if(event.key == 'a'){
-		player.velocity.x = 0;
+
+	if (enemy.keys.moveX.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+		enemy.velocity.x = -2;
+	}
+	else if (enemy.keys.moveX.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+		enemy.velocity.x = 2;
+	}
+}
+
+// key binding
+window.addEventListener('keydown', function (event) {
+	if (Object.keys(player.keys.moveX).includes(event.key)) {
+		player.keys.moveX[event.key].pressed = true;
+		player.lastKey = event.key;
+	}
+	if (Object.keys(enemy.keys.moveX).includes(event.key)) {
+		enemy.keys.moveX[event.key].pressed = true;
+		enemy.lastKey = event.key;
+	}
+
+	if (event.key == 'w') {
+		if (player.position.y + player.parameters.height >= canvas.height) {
+			player.position.y = player.parameters.jumpHeight;
+		}
+	}
+	if (event.key == 'ArrowUp') {
+		if (enemy.position.y + enemy.parameters.height >= canvas.height) {
+			enemy.position.y = enemy.parameters.jumpHeight;
+		}
 	}
 });
+
+
+window.addEventListener('keyup', function (event) {
+	if (Object.keys(player.keys.moveX).includes(event.key)) {
+		player.keys.moveX[event.key].pressed = false;
+	}
+	if (Object.keys(enemy.keys.moveX).includes(event.key)) {
+		enemy.keys.moveX[event.key].pressed = false;
+	}
+});
+
+animate();
